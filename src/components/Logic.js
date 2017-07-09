bfd792e101a67a05ae4fb3d03c535cc7242ce64a
Harinder Kaur <snazyahuja11@gmail.com>
2017-07-09 10:37:07 -0400

    Separate components folder created

---
+++ src/components/Logic.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+ export class Logic extends Component{
+	render(){
+		return(
+			<div>
+			<button >GST rates on Goods</button>
+        	<button >GST rates on Services</button>
+			</div>);
+	}
+}
+
+export default Logic;
